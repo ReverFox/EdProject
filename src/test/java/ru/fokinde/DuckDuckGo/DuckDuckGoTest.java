da710242507052149192ead3e7cb6ba8dc4eeb47
ReverFox <reverfox@mail.ru>
2025-08-23 20:54:24 +0300

    feat. Add init() method

--- src/test/java/ru/fokinde/DuckDuckGo/DuckDuckGoTest.java
+++ src/test/java/ru/fokinde/DuckDuckGo/DuckDuckGoTest.java
@@ -5,16 +5,25 @@ import static com.codeborne.selenide.Condition.*;
 import static ru.fokinde.DuckDuckGo.MainPage.*;
 
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.TmsLink;
+import org.aspectj.lang.annotation.Before;
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class DuckDuckGoTest {
 
+    @BeforeClass
+    public void init() {
+        WebDriverManager.chromedriver().setup();
+        Configuration.headless = true;
+        Configuration.browser = "chrome";
+    }
+
     @Test(description = "Проверка отображения поиска на главной странице")
     @TmsLink("TMS-001")
     public void mainPageTest() {
-        Configuration.headless = true;
         open("https://duckduckgo.com");
         $(By.cssSelector(SEARCH_BOX)).should(exist);
         $(By.cssSelector(ABOUT_REF)).should(exist);
