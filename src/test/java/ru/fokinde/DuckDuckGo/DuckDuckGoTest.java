c42f4c669ca96490f7ef3fbfa1f34bdf8b6fd49e
ReverFox <reverfox@mail.ru>
2025-08-28 08:05:57 +0300

    fix. Add new test mainPageSearchBoxVisibleTest()

--- src/test/java/ru/fokinde/DuckDuckGo/DuckDuckGoTest.java
+++ src/test/java/ru/fokinde/DuckDuckGo/DuckDuckGoTest.java
@@ -9,7 +9,8 @@ import org.testng.annotations.Test;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.fokinde.DuckDuckGo.MainPage.*;
+import static ru.fokinde.DuckDuckGo.MainPage.ABOUT_REF;
+import static ru.fokinde.DuckDuckGo.MainPage.SEARCH_BOX;
 
 public class DuckDuckGoTest {
 
@@ -20,14 +21,21 @@ public class DuckDuckGoTest {
         Configuration.remote = "http://192.168.56.1:4444/";
     }
 
-    @Test(description = "Проверка отображения поиска на главной странице")
+    @Test(description = "Поисковая строка отображается на главной странице")
     @TmsLink("TMS-001")
-    public void mainPageTest() {
+    public void mainPageSearchBoxVisibleTest() {
         open("https://duckduckgo.com");
         $(By.cssSelector(SEARCH_BOX)).should(exist);
+        //$(By.cssSelector(ABOUT_REF)).should(exist);
+        //$(By.cssSelector(SEARCH_BUTTON)).should(exist);
+        //$(By.cssSelector(AI_CHAT_BUTTON)).should(exist);
+        //$(By.cssSelector(SIDE_BAR_BUTTON)).should(exist);
+    }
+
+    @Test(description = "Проверка отображения поиска на главной странице")
+    @TmsLink("TMS-001")
+    public void mainPageAboutRefVisibleTest() {
+        open("https://duckduckgo.com");
         $(By.cssSelector(ABOUT_REF)).should(exist);
-        $(By.cssSelector(SEARCH_BUTTON)).should(exist);
-        $(By.cssSelector(AI_CHAT_BUTTON)).should(exist);
-        $(By.cssSelector(SIDE_BAR_BUTTON)).should(exist);
     }
 }
