ea25fc1e13504ef0f9c84870cba14f19a064be72
ReverFox <reverfox@mail.ru>
2025-09-11 21:52:19 +0300

    feat. Add new class DesktopSearchResultPage

---
+++ src/main/java/ru/fokinde/DuckDuckGo/DesktopSearchResultPage.java
@@ -0,0 +1,14 @@
+package ru.fokinde.DuckDuckGo;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DesktopSearchResultPage {
+    public final SelenideElement SEARCH_STRING_INPUT = $("[id=\"search_form_input\"]");
+    public final SelenideElement SEARCH_STRING_RESET_BUTTON = $("button[type=\"reset\"]");
+    public final SelenideElement SEARCH_STRING_SEARCH_BUTTON = $("button[type=\"search\"]");
+    public final ElementsCollection SEARCH_RESULTS = $$(".react-results--main li[data-layout=\"organic\"]");
+}
