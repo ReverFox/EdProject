bb3181f919874c8cf29070310b23432c3fda8da1
ReverFox <reverfox@mail.ru>
2025-09-11 21:51:58 +0300

    feat. Add new method

--- src/main/java/ru/fokinde/DuckDuckGo/DesktopMainPage.java
+++ src/main/java/ru/fokinde/DuckDuckGo/DesktopMainPage.java
@@ -5,17 +5,18 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
 
 public class DesktopMainPage {
     public static final String DESKTOP_WRAPPER_CSS = "[class*=\"header_headerWrapDesktop\"]";
 
-    public static final SelenideElement ABOUT_REF = $(DESKTOP_WRAPPER_CSS + " [class*=\"header_logoText\"]");
-    public static final SelenideElement AI_CHAT_REF = $(DESKTOP_WRAPPER_CSS + " [data-testid=\"aichat-button\"]");
-    public static final SelenideElement SEARCH_STRING_INPUT = $("[class*=\"searchbox_searchbox\"] input");
-    public static final SelenideElement SEARCH_STRING_BUTTON = $("[class*=\"searchbox_searchButton\"]");
-    public static final SelenideElement SIDE_MENU_OPEN_BUTTON = $(DESKTOP_WRAPPER_CSS + " button[data-testid=\"sidemenu-button\"");
-    public static final SelenideElement SIDE_MENU_CLOSE_BUTTON = $(DESKTOP_WRAPPER_CSS + " button[class*=\"sideMenu_closeButton\"]");
-    public static final SelenideElement SIDE_MENU = $(DESKTOP_WRAPPER_CSS + " [data-testid=\"sidemenu\"]");
+    public final SelenideElement ABOUT_REF = $(DESKTOP_WRAPPER_CSS + " [class*=\"header_logoText\"]");
+    public final SelenideElement AI_CHAT_REF = $(DESKTOP_WRAPPER_CSS + " [data-testid=\"aichat-button\"]");
+    public final SelenideElement SEARCH_STRING_INPUT = $("[class*=\"searchbox_searchbox\"] input");
+    public final SelenideElement SEARCH_STRING_BUTTON = $("[class*=\"searchbox_searchButton\"]");
+    public final SelenideElement SIDE_MENU_OPEN_BUTTON = $(DESKTOP_WRAPPER_CSS + " button[data-testid=\"sidemenu-button\"");
+    public final SelenideElement SIDE_MENU_CLOSE_BUTTON = $(DESKTOP_WRAPPER_CSS + " button[class*=\"sideMenu_closeButton\"]");
+    public final SelenideElement SIDE_MENU = $(DESKTOP_WRAPPER_CSS + " [data-testid=\"sidemenu\"]");
     
     
     public void openSideMenu() {
@@ -28,5 +29,11 @@ public class DesktopMainPage {
         SIDE_MENU_CLOSE_BUTTON
                 .shouldBe(visible)
                 .click();
+    }
+
+    public DesktopSearchResultPage search(String search_query) {
+        SEARCH_STRING_INPUT.type(search_query);
+        SEARCH_STRING_BUTTON.click();
+        return page(DesktopSearchResultPage.class);
     }
 }
